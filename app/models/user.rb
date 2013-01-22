9c60e838387ae11d08deb92acbd58d462796332f
Jorge Moscat <jorgemoscat@gmail.com>
2013-01-22 10:20:22 -0800

    Working Devise on Mongo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,30 @@
-class User < ActiveRecord::Base
+class User
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :user_uid, type: String
+  field :fb_token, type: String
+  field :authentication_token, :type => String
+  field :name, type: String
+  field :email, type: String
+  field :location_name, type: String
+  field :location_id, type: String
+  field :DOB, type: Date
+
+  field :friend_count, type: Integer
+  field :fb_friends, type: Array
+  field :thanxup_friends, type: Array
+  field :NN_thanxup_friends, type: Array
+
+  field :influence, type: Float
+  field :iphone_id, type: Integer # for push notifications
+  field :android_id, type: Integer # for push notifications
+  index({user_uid: 1}, {unique: true})
+
+  embeds_many :cupons
+  embeds_many :visits
+
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -16,12 +42,41 @@ class User < ActiveRecord::Base
   	if profile.nil? or profile["id"] != fb_uid
   		return nil
   	else
-      Profile.new_profile(fb_uid,fb_token)
-  		new_user = User.new()
-  		new_user.uid = fb_uid
-  		new_user.fb_access_token = fb_token
+  		new_user = User.new
+  		new_user.user_uid = fb_uid
+  		new_user.fb_token = fb_token
   		new_user.save
+      User.get_facebook_data(new_user)
   		return new_user
   	end
   end
+
+  def self.get_facebook_data(user)
+    @graph = Koala::Facebook::API.new(user.fb_token)
+    profile = @graph.get_object("me")
+    user.email = profile["email"]
+    user.name = profile["name"]
+    user.location_name = profile["location"]["name"]
+    user.location_id = profile["location"]["id"]
+    user.DOB = Date.strptime(profile["birthday"], '%m/%d/%Y')
+
+    #get_friends
+    friends = Array.new
+    @graph.get_connections("me","friends",:fields =>"id").each do |x|
+      friends << x["id"]
+    end
+
+    user.fb_friends = friends
+    num = @graph.fql_query("SELECT friend_count FROM user WHERE uid = me()")
+    user.friend_count = num.first["friend_count"]
+    user.save
+  end
+
+
+
+
+
+
+
+
 end
