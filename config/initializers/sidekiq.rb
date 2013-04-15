84b74dd095368ab5dbcf3e09f1fd27417320fed5
Jorge Moscat <jorgemoscat@gmail.com>
2013-04-14 21:25:51 -0700

    Working all PROD stuff

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,9 @@
+if Rails.env.production? || Rails.env.staging?
+  Sidekiq.configure_server do |config|
+    config.redis = { url:'redis://thanxup-worker:19032011@pub-redis-10664.us-east-1-3.2.ec2.garantiadata.com:10664'}
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url:'redis://thanxup-worker:19032011@pub-redis-10664.us-east-1-3.2.ec2.garantiadata.com:10664' }
+  end
+end
